6ee509f2f597515eff046bca325a8363d5a49e89
Thijs van der Ree <99053846@mydavinci.nl>
2018-10-16 14:37:29 +0200

    Added some more things

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var fris = 0;
+var bier = 0;
+var wijn = 0;
+var snack = 0;
+
+var frisprijs = 0;
+var bierprijs = 0;
+var wijnprijs = 0;
+var snackprijs = 0;
+
+var totaalprijs = 0;
+
+const FRISPRIJS = 2.00;
+const BIERPRIJS = 4.00;
+const WIJNPRIJS = 5.00;
+const SNACKPRIJS = 0.20;
+
+function go(bestelling) {
+	var bestelling = prompt("Wat voor bestelling wilt u maken?", "Fris / bier / wijn / snack").toLocalLowerCase;
+} if (bestelling == "fris") {
+	
+}
